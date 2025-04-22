8a0362ed6f405092fa0151ba3256ccc65203438f
Musficc <t81218942@gmail.com>
2025-04-22 14:39:05 +0300

    small adjustments

--- modmain.lua
+++ modmain.lua
@@ -10,17 +10,13 @@ else RemapSoundEvent("rockbeefalo/beefalo/curious", "dontstarve/beefalo/curious"
 
 AddPrefabPostInit("beefalo", function(inst)
     local lastBank = "beefalo"
-
     inst:DoPeriodicTask(0.1, function()
-        if not inst:IsValid() or not inst.AnimState then
-            return
-        end
-
-        local anim = inst.AnimState
-        local newBank = anim:IsCurrentAnimation("alert_idle") and "rockbeefalo" or "beefalo"
-        if newBank ~= lastBank then
-            anim:SetBank(newBank)
-            lastBank = newBank
+        if inst:IsValid() and inst.AnimState then
+            local newBank = inst.AnimState:IsCurrentAnimation("alert_idle") and "rockbeefalo" or "beefalo"
+            if newBank ~= lastBank then
+                inst.AnimState:SetBank(newBank)
+                lastBank = newBank
+            end
         end
     end)
 end)
