dd4bd0a2df9d67e6a110b27b021d406ee61e09aa
Musficc <t81218942@gmail.com>
2025-05-07 17:06:49 +0300

    not working with caves fixed

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 name = "Rock Beefalo"
-version = "1.0.0"
+version = "1.0.1"
 
 description = "Change look and sound that beefalo makes when you approach"
 author = "Temort"
--- modmain.lua
+++ modmain.lua
@@ -1,34 +1,22 @@
 Assets = {
     Asset("ANIM", "anim/rockbeefalo_actions_bank.zip"),
     Asset("SOUNDPACKAGE", "sound/rockbeefalo.fev"),
-	Asset("SOUND", "sound/rockbeefalo.fsb"),
+    Asset("SOUND", "sound/rockbeefalo.fsb"),
 }
 
 local useVanillaSound = GetModConfigData("VANILLA_SOUND")
-if not useVanillaSound then RemapSoundEvent("dontstarve/beefalo/curious", "rockbeefalo/beefalo/curious")
-else RemapSoundEvent("dontstarve/beefalo/curious", "dontstarve/beefalo/curious") end
+if not useVanillaSound then
+    RemapSoundEvent("dontstarve/beefalo/curious", "rockbeefalo/beefalo/curious")
+else
+    RemapSoundEvent("dontstarve/beefalo/curious", "dontstarve/beefalo/curious")
+end
 
 AddPrefabPostInit("beefalo", function(inst)
-    local lastBank = "beefalo"
-    
-    local function UpdateBank(inst)
-        if not (inst and inst:IsValid()) then
-            return
+    inst:DoPeriodicTask(0.01, function()
+        if inst.AnimState:IsCurrentAnimation("alert_idle") then
+            inst.AnimState:SetBank("rockbeefalo")
+        else
+            inst.AnimState:SetBank("beefalo")
         end
-
-        local animState = inst.AnimState
-        if not animState then
-            return
-        end
-
-        local newBank = animState:IsCurrentAnimation("alert_idle") and "rockbeefalo" or "beefalo"
-
-        if newBank ~= lastBank then
-            animState:SetBank(newBank)
-            lastBank = newBank
-        end
-    end
-
-    inst:ListenForEvent("animover", UpdateBank)
-    inst:ListenForEvent("newstate", UpdateBank)
+    end)
 end)
